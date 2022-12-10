ecaa4901745e78ea8dc8e12cdca717c6e883a1e6
roy-west <110616550+roy-west@users.noreply.github.com>
2022-12-10 16:30:01 +0800

    new commit

--- stores/admin.py
+++ stores/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Shop, Category, Product
+from .models import Shop, Category, Product, Supplies, Sales
 # Register your models here.
 admin.site.register(Shop)
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Supplies)
+admin.site.register(Sales)
+
 
